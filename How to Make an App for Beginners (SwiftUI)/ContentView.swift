efec2ecc25645e97040085bf6e8ba5594b17fb59
YoonDii <dbswl9506@naver.com>
2022-10-04 22:37:29 +0900

    How to Make an App for Beginners (SwiftUI) 11강까지

--- How to Make an App for Beginners (SwiftUI)/ContentView.swift
+++ How to Make an App for Beginners (SwiftUI)/ContentView.swift
@@ -8,17 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    
+    @State var playerCard = "card5"
+    @State  var cpuCard = "card9"
+    @State var playerScore = 0
+    @State  var cpuScore = 0
+     var body: some View {
+        ZStack{
+            Image("background")
+                .ignoresSafeArea()
+            
+            
+            VStack() {
+                Spacer()
+                Image("logo")
+                
+                Spacer()
+                HStack() {
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                Spacer()
+                Button(action: {
+                    //Generate a random number between 2 and 14
+                    let playerRand = Int.random(in: 2...14)
+                    let cpuRand = Int.random(in: 2...14)
+                    
+                    //update the cards
+                    playerCard = "card" + String(playerRand)
+                    cpuCard = "card" + String(cpuRand)
+                    
+                    //update the score
+                    // playerScore += 1
+                    //  cpuScore += 1
+                    
+                }, label: {
+                    Image("dealbutton")})
+                
+                Spacer()
+                HStack() {
+                    Spacer()
+                    VStack{
+                        Text("Player")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 2.0)
+                        Text(String(playerScore))
+                            .font(.subheadline)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                    VStack{
+                        Text(String(cpuScore))
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 2.0)
+                        Text("0")
+                            .font(.subheadline)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }
+            
         }
-        .padding()
-    }
+            }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
