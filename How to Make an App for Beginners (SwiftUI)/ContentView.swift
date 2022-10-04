ad8f7a4255ca128b4faa2a476b0252f6e3593f25
YoonDii <dbswl9506@naver.com>
2022-10-04 22:22:53 +0900

    Initial Commit

---
+++ How to Make an App for Beginners (SwiftUI)/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  How to Make an App for Beginners (SwiftUI)
+//
+//  Created by 김윤지 on 2022/10/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ How to Make an App for Beginners (SwiftUI)/How_to_Make_an_App_for_Beginners__SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  How_to_Make_an_App_for_Beginners__SwiftUI_App.swift
+//  How to Make an App for Beginners (SwiftUI)
+//
+//  Created by 김윤지 on 2022/10/04.
+//
+
+import SwiftUI
+
+@main
+struct How_to_Make_an_App_for_Beginners__SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
